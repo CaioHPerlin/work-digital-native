2324402e17cfafb98bbc8f496c320c0afe91c4b0
willian <williancardoso00004@gmail.com>
2024-05-29 22:08:56 -0400

    feat: select tipos de serviço

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import { Button, View, Text, } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import AccountScreen from './screens/AccountScreen';
 import ExitScreen from './screens/ExitScreen';
 
-
+const KbStyles = {
+  white: '#FFFFFF',
+  fundoHeader: '#FCb773',
+  headerItem: '#FFC88d'
+};
 
 const Drawer = createDrawerNavigator();
 export default function App() {
@@ -15,9 +18,23 @@ export default function App() {
 < >
 
   <Drawer.Navigator initialRouteName="Home"
-  
+    screenOptions={{
+      drawerStyle: {
+        width: 240
+      },
+      headerStyle: {
+        height: 80,
+        backgroundColor: KbStyles.fundoHeader
+      },
+
+      headerTitleStyle: {
+        color: KbStyles.white
+      },
+      drawerActiveBackgroundColor : KbStyles.headerItem,
+      drawerActiveTintColor: "white"
+    }}
   >
-        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen  name="Home" component={HomeScreen} />
         <Drawer.Screen name="Configurações" component={SettingsScreen} />
         <Drawer.Screen name="Conta" component={AccountScreen} />
         <Drawer.Screen name="Sair" component={ExitScreen} />
--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,17 +1,41 @@
-import * as React from 'react';
-import { Button, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 export default function HomeScreen() {
+  const [selectedValue, setSelectedValue] = useState("item1");
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        
-        title="Go to notifications"
-      />
+    <View style={styles.container}>
+      <View style={styles.pickerContainer}>
+        <Text>TIPOS DE SERVIÇO</Text>
+        <Picker
+          selectedValue={selectedValue}
+          style={styles.picker}
+          onValueChange={(itemValue) => setSelectedValue(itemValue)}
+        >
+          <Picker.Item label="Item 1" value="item1" />
+          <Picker.Item label="Item 2" value="item2" />
+          <Picker.Item label="Item 3" value="item3" />
+          <Picker.Item label="Item 4" value="item4" />
+          <Picker.Item label="Item 5" value="item5" />
+          <Picker.Item label="Item 6" value="item6" />
+        </Picker>
+      </View>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
 
-
+    backgroundColor: '#FFF',
+  },
+  pickerContainer: {
+    alignSelf: 'flex-end', // Alinhar à direita
+  },
+  picker: {
+    height: 50,
+    width: 150,
+  },
+});
