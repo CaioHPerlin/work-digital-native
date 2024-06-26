1710f02cd6cbf55a26befacf14a8f2cbe45595e4
willian <williancardoso00004@gmail.com>
2024-06-26 12:36:37 -0400

    feat:  ChangePassword

--- app/screens/opcoesConfig/ChangePassword.tsx
+++ app/screens/opcoesConfig/ChangePassword.tsx
@@ -1,37 +1,137 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import * as React from "react";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { TextInput } from "react-native-paper";
+import Layout from "@/app/components/Layout";
 
-const ChangePassword = ({ setCurrentScreen }) => {
+export default function ChangePassword({ navigation }) {
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleCancel = () => {
+    // Implementar lógica de cancelamento
+    navigation.goBack();
+  };
+
+  const handleChangePassword = () => {
+    // Implementar lógica de alteração de senha
+    if (newPassword !== confirmPassword) {
+      alert("As senhas não coincidem");
+      return;
+    }
+    // Lógica para alterar a senha
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Alterar Senha</Text>
-      <TouchableOpacity style={styles.button} onPress={() => setCurrentScreen('ConfigApp')}>
-        <Text style={styles.buttonText}>Voltar para Configurações</Text>
-      </TouchableOpacity>
-    </View>
+    <Layout>
+      <View style={styles.container}>
+        <ScrollView>
+          <Text style={styles.title}>Alterar Senha</Text>
+
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Senha Atual</Text>
+            <TextInput
+              label="Senha Atual"
+              value={oldPassword}
+              onChangeText={(text) => setOldPassword(text)}
+              secureTextEntry
+              style={styles.input}
+              underlineColor="transparent"
+              activeUnderlineColor="black"
+            />
+          </View>
+
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Nova Senha</Text>
+            <TextInput
+              label="Nova Senha"
+              value={newPassword}
+              onChangeText={(text) => setNewPassword(text)}
+              secureTextEntry
+              style={styles.input}
+              underlineColor="transparent"
+              activeUnderlineColor="black"
+            />
+          </View>
+
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Confirmar Nova Senha</Text>
+            <TextInput
+              label="Confirmar Nova Senha"
+              value={confirmPassword}
+              onChangeText={(text) => setConfirmPassword(text)}
+              secureTextEntry
+              style={styles.input}
+              underlineColor="transparent"
+              activeUnderlineColor="black"
+            />
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={handleCancel}>
+              <Text style={styles.buttonText}>Voltar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleChangePassword}>
+              <Text style={styles.buttonText}>Alterar Senha</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </View>
+    </Layout>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center' 
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
-    fontSize: 24,
-    marginBottom: 20
+    fontSize: 34,
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  inputGroup: {
+    marginBottom: 10,
+    width: "100%",
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  input: {
+    backgroundColor: "#fef5eb",
+    marginBottom: 10,
+    borderColor: "#FFC88d",
+    borderWidth: 1,
+    color: "#000000",
+    borderRadius: 5,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+    width: "100%",
   },
   button: {
-    backgroundColor: '#1E90FF',
+    backgroundColor: "#FFC88d",
     padding: 10,
-    marginTop: 10,
-    borderRadius: 5
+    borderRadius: 5,
+    alignItems: "center",
+    flex: 1,
+    marginHorizontal: 5,
   },
   buttonText: {
-    color: '#FFFFFF',
-    fontSize: 16
-  }
+    color: "#000",
+    fontWeight: "bold",
+  },
 });
-
-export default ChangePassword;
