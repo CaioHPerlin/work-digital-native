503e1f7a3a7d1208baa473acc4f08dfaf51b9d21
willian <williancardoso00004@gmail.com>
2024-07-01 07:26:29 -0400

    fix: configApp

--- app/screens/ConfigApp.tsx
+++ app/screens/ConfigApp.tsx
@@ -1,56 +1,67 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+//import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-
-const ConfigApp = ({navigation}) => {
-
-
-        return (
-          <View style={styles.container}>
-            <Text style={styles.title}>Configurações do Aplicativo</Text>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('DadosPessoais')}>
-              <Text style={styles.buttonText}>Dados Pessoais</Text>
-              
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('PersonalInfo')}>
-              <Text style={styles.buttonText}>testeS</Text>
-              
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('BecomeAutonomo')}>
-              <Text style={styles.buttonText}>Tornar-se Autônomo</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('ChangePassword')}>
-              <Text style={styles.buttonText}>Alterar Senha</Text>
-            </TouchableOpacity>
-,
-          </View>
-        );
-    }
-  
-
+const ConfigApp = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Configurações do Aplicativo</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("DadosPessoais")}
+      >
+        <Text style={styles.buttonText}>Dados Pessoais</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("PersonalInfo")}
+      >
+        <Text style={styles.buttonText}>teste</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("BecomeAutonomo")}
+      >
+        <Text style={styles.buttonText}>Tornar-se Autônomo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("ChangePassword")}
+      >
+        <Text style={styles.buttonText}>Alterar Senha</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Home")}
+      >
+        <Text style={styles.buttonText}>Voltar</Text>
+      </TouchableOpacity>
+      
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center' 
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     fontSize: 24,
-    marginBottom: 20
+    marginBottom: 20,
   },
   button: {
-    backgroundColor: '#FFC88d',
+    backgroundColor: "#FFC88d",
     padding: 15,
     borderRadius: 5,
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 10,
-    width: '80%',
+    width: "80%",
   },
   buttonText: {
-    color: '#000',
-    fontWeight: 'bold',
-  }
+    color: "#000",
+    fontWeight: "bold",
+  },
 });
 
 export default ConfigApp;
